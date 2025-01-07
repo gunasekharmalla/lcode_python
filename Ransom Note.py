825c55972a4fcc8d8fd22bf3de31e762edc7e325
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-01-07 20:27:45 +0530

    Update Ransom Note.py

--- Ransom Note.py
+++ Ransom Note.py
@@ -1,1 +1,12 @@
+from collections import Counter
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        if ransomNote == magazine:
+            return True 
+        mag = Counter(magazine)
+        ran = Counter(ransomNote)
+        for char, count in ran.items():
+            if mag[char] < count:
+                return False 
+        return True
 
