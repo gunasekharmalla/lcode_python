40321909c926bc0e8abe53d4c6f925d57908826c
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-01-10 20:52:56 +0530

    Update Character Frequency.py

--- Character Frequency.py
+++ Character Frequency.py
@@ -1,1 +1,11 @@
 
+from collections import Counter 
+class Solution(object):
+    def areOccurrencesEqual(self, s):
+        if not s:
+            return False
+        count = Counter(s)
+        val = [val for val in count.values()]
+        same = all(x == val[0] for x in val)
+        return same
+
