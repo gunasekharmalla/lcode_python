2acfdb6c7d8c6b5cf45364c7835eddbfe0037174
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-01-21 10:13:01 +0530

    Update Harshad Number.py

--- Harshad Number.py
+++ Harshad Number.py
@@ -1,1 +1,13 @@
 
+class Solution(object):
+    def sumOfTheDigitsOfHarshadNumber(self, x):
+        val = x;sm=0
+        while(x!=0):
+            rm = x % 10
+            sm += rm
+            x /= 10 
+        if val % sm == 0:
+            return sm
+        return -1
+            
+        
