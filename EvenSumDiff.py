bc259f25498f9b5244b7c4c194cf2875f17c3380
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-01-26 18:08:19 +0530

    Update EvenSumDiff.py

--- EvenSumDiff.py
+++ EvenSumDiff.py
@@ -1,1 +1,13 @@
 
+class Solution(object):
+    def countPartitions(self, nums):
+        c = 0
+        for i in range(1, len(nums)):
+            ls1 = nums[:i]
+            ls2 = nums[i:]
+            s1 = sum(ls1)
+            s2 = sum(ls2)
+            if (s1 - s2) % 2 == 0:
+                c += 1
+        return c
+         
