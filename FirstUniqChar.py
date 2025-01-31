f946ccd442fc1e8a32612e86fa6c5844007a10ca
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-01-31 21:12:53 +0530

    Update FirstUniqChar.py

--- FirstUniqChar.py
+++ FirstUniqChar.py
@@ -1,1 +1,11 @@
 
+class Solution(object):
+    def firstUniqChar(self, name):
+     dictionary = {char :0 for char in name}
+     for char in name:
+        if char in dictionary:
+            dictionary[char] += 1
+     for element in name:
+        if dictionary[element] ==1:
+            return name.find(element)
+     return -1
