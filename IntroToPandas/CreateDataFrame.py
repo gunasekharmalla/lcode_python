da8a1d5ba5d2a1c2ffc04eab8d79e8a83486cddf
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-02-02 22:00:39 +0530

    Update CreateDataFrame.py

--- IntroToPandas/CreateDataFrame.py
+++ IntroToPandas/CreateDataFrame.py
@@ -1,1 +1,5 @@
+import pandas as pd
 
+def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
+    sam = pd.DataFrame(data = student_data, columns = ['student_id','age'])
+    return sam
