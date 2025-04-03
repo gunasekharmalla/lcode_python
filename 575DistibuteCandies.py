262d20549c8bcfe76a116c7eae056359b19cd7cb
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-04-03 21:26:33 +0530

    Update 575DistibuteCandies.py

--- 575DistibuteCandies.py
+++ 575DistibuteCandies.py
@@ -1,1 +1,11 @@
+class Solution(object):
+    def distributeCandies(self, candyType):
+        n = len(candyType)//2
+        n1 = len(set(candyType))
+        return min(n,n1)
 
+Input: candyType = [1,1,2,2,3,3]
+Output: 3
+
+Input: candyType = [1,1,2,3]
+Output: 2
