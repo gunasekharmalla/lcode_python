279af92e4feca86d16b5759bef63dd936eb97aab
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-04-06 10:40:07 +0530

    Update 141LlistCycle.py

--- 141LlistCycle.py
+++ 141LlistCycle.py
@@ -1,1 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
+class Solution(object):
+    def hasCycle(self, head):
+        if head is None:
+            return 
+        fast = slow = head 
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+
+
+Input: head = [1,2], pos = 0
+Output: true
+
+ Input: head = [1], pos = -1
+Output: false       
