3f471c8f8ee389e9ff668adbb2b812734d331bc6
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-04-07 22:51:28 +0530

    Update 53MaxSubArr.py

--- 53MaxSubArr.py
+++ 53MaxSubArr.py
@@ -1,1 +1,8 @@
-
+class Solution(object):
+    def maxSubArray(self, nums):
+        max_ending = max_sum = nums[0]
+        for left in range(1,len(nums)):
+            max_ending = max(nums[left], (max_ending+nums[left]))
+            max_sum = max(max_sum, max_ending)
+        return max_sum
+        
