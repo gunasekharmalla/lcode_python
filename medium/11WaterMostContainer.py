29e5fb320d32c9fb772165034484c16c48afccf2
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-04-08 23:53:41 +0530

    Update 11WaterMostContainer.py

--- medium/11WaterMostContainer.py
+++ medium/11WaterMostContainer.py
@@ -1,1 +1,19 @@
 
+class Solution(object):
+    def maxArea(self, height):
+        #height = [1,8,6,2,5,4,8,3,7]
+        left, right = 0, len(height)-1
+        max_area = float('-inf')
+        while left < right:
+            min_ = min(height[left], height[right])
+            width = right - left
+            area = min_ * width
+            if area > max_area:
+                max_area = area
+            if height[left] <= height[right]:
+                left += 1
+            else:
+                right -= 1
+        return max_area
+
+        
