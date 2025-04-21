1d471ac7dbe4232effb75a2b587167baeaf61391
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-04-21 20:16:23 +0530

    Update 80.py

--- medium/80.py
+++ medium/80.py
@@ -1,1 +1,18 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        j, i, cnt = 0, 1, 0
+        while i < len(nums):
+            if nums[i] == nums[j] and cnt == 0:
+                j += 1
+                nums[j] = nums[i]
+                cnt += 1
+                i += 1
+            elif nums[i] == nums[j] and cnt > 0:
+                i += 1
+            else:
+                j += 1
+                nums[j] = nums[i]
+                cnt = 0
+                i += 1
+        return j + 1
 
