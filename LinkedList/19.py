393932b9b9d4b11464a496bc88cafc4986fa6682
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-04-26 17:36:54 +0530

    Update 19.py

--- LinkedList/19.py
+++ LinkedList/19.py
@@ -1,1 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        if not head:
+            return None
+        dummy = ListNode(0)
+        dummy.next = head
+        slow = fast = dummy
+
+        for _ in range(n+1):
+            if fast:
+                fast = fast.next
+        
+        while fast:
+            slow = slow.next
+            fast = fast.next
+        slow.next = slow.next.next
+
+        return dummy.next
 
