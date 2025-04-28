87d9f736c103ed6ceaa3f648176627aa49b26d83
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-04-28 21:49:25 +0530

    Update 152.py

--- medium/152.py
+++ medium/152.py
@@ -1,1 +1,14 @@
+class Solution(object):
+    def maxProduct(self, nums):
+        res = max(nums) 
+        cur_max, cur_min = 1, 1
+        for ele in nums:
+            if ele == 0:
+                cur_max, cur_min = 1, 1
+                continue
+            temp = cur_max * ele
+            cur_max = max(ele, temp, cur_min * ele)
+            cur_min = min(ele, temp, cur_min * ele)
+            res = max(res, cur_max)
+        return res
 
