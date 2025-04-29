41b7434972032f587079f77c76b2627e1da2b796
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-04-29 21:37:59 +0530

    Update 287.py
    find duplicate number

--- medium/287.py
+++ medium/287.py
@@ -1,1 +1,11 @@
+class Solution(object):
+    def findDuplicate(self, nums):
+        freq = {}
+        for ele in nums:
+            freq[ele] = freq.get(ele, 0) + 1
+        for key, val in freq.items():
+            if val > 1:
+                return key
 
+nums = [1,3,4,2,2]
+output = 2
