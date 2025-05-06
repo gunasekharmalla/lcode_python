57f2651af84fb7101892fa9166f31e11a6f0eacf
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-05-06 22:37:32 +0530

    Update 347.py
    top k elements

--- medium/347.py
+++ medium/347.py
@@ -1,1 +1,7 @@
+from collections import Counter as cnt
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        count = cnt(nums)
+        return [item for item, _ in count.most_common(k)]
+
 
