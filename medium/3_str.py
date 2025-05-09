fa1e2a98ffca2aadc0437984cff433916712399e
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-05-09 17:41:30 +0530

    Update 3_str.py

--- medium/3_str.py
+++ medium/3_str.py
@@ -1,1 +1,12 @@
-
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        s1 = set()
+        j = max_len = 0
+        for i in range(len(s)):
+            while s[i] in s1:
+                s1.remove(s[j])
+                j+=1
+            s1.add(s[i])
+            max_len = max(max_len, i - j +1)
+        return max_len
+        
