6d11c040d16d95ea898822c8e1567da03eada443
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-05-29 22:33:45 +0530

    Update LongestPalindromeSubStr.py

--- medium/LongestPalindromeSubStr.py
+++ medium/LongestPalindromeSubStr.py
@@ -1,1 +1,24 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        def spaceAround(left, right):
+            while left >= 0 and right <= len(s)-1 and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left+1 : right]
+        length = ""
+        for i in range(len(s)):
+            odd = spaceAround(i,i)
+            even = spaceAround(i, i+1)
+            length = max(length, odd, even, key = len)
+        return length
 
+        
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+Example 2:
+
+Input: s = "cbbd"
+Output: "bb"
