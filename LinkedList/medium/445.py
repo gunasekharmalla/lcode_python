e8b3e80430b17a24b5edd0f42ad0ca24d52c3c83
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-05-31 22:30:34 +0530

    Update 445.py

--- LinkedList/medium/445.py
+++ LinkedList/medium/445.py
@@ -1,1 +1,41 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        carry = 0
+        prev = None
+        stack1 = []
+        stack2 = []
 
+        while l1:
+            stack1.append(l1.val)
+            l1 = l1.next
+        while l2:
+            stack2.append(l2.val) 
+            l2 = l2.next
+        
+        while stack1 or stack2 or carry:
+            val1 = stack1.pop() if stack1 else 0
+            val2 = stack2.pop() if stack2 else 0
+
+            total = val1 + val2 + carry 
+            carry = total // 10
+
+            current = ListNode(total % 10 )
+            current.next = prev
+            prev = current
+        return prev
+
+Input: l1 = [7,2,4,3], l2 = [5,6,4]
+Output: [7,8,0,7]
+Example 2:
+
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [8,0,7]
+Example 3:
+
+Input: l1 = [0], l2 = [0]
+Output: [0]
