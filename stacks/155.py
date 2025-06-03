c1be419465295cdc8b86587932b60db243a14555
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-06-03 21:38:34 +0530

    Update 155.py
    min stack - leetcode

--- stacks/155.py
+++ stacks/155.py
@@ -1,1 +1,41 @@
+class MinStack(object):
 
+    def __init__(self):
+        self.stack = []
+        
+
+    def push(self, val):
+        return self.stack.append(val)
+        
+        
+    def pop(self):
+        return self.stack.pop()
+        
+
+    def top(self):
+        return self.stack[-1]
+        
+
+    def getMin(self):
+        mini = self.stack[0]
+        for i in range(len(self.stack)):
+            if self.stack[i] < mini:
+                mini = self.stack[i]
+        return mini
+
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
