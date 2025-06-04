edfd07e50c02aec8336179aa5340f7079c65e33a
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-06-04 21:33:40 +0530

    Update 1381.py

--- stacks/1381.py
+++ stacks/1381.py
@@ -1,1 +1,54 @@
+class CustomStack(object):
 
+    def __init__(self, maxSize):
+        self.stack = []
+        self.maxSize = maxSize
+        
+
+    def push(self, x):
+        if len(self.stack) == self.maxSize:
+            return -1
+        else:
+            self.stack.append(x)
+        
+
+    def pop(self):
+        if not self.stack:
+            return -1 
+        else:
+            return self.stack.pop()
+        
+
+    def increment(self, k, val):
+        for i in range(min(k,len(self.stack))):
+            self.stack[i] += val 
+
+
+# Your CustomStack object will be instantiated and called as such:
+# obj = CustomStack(maxSize)
+# obj.push(x)
+# param_2 = obj.pop()
+# obj.increment(k,val)
+
+
+Example 1:
+
+Input
+["CustomStack","push","push","pop","push","push","push","increment","increment","pop","pop","pop","pop"]
+[[3],[1],[2],[],[2],[3],[4],[5,100],[2,100],[],[],[],[]]
+Output
+[null,null,null,2,null,null,null,null,null,103,202,201,-1]
+Explanation
+CustomStack stk = new CustomStack(3); // Stack is Empty []
+stk.push(1);                          // stack becomes [1]
+stk.push(2);                          // stack becomes [1, 2]
+stk.pop();                            // return 2 --> Return top of the stack 2, stack becomes [1]
+stk.push(2);                          // stack becomes [1, 2]
+stk.push(3);                          // stack becomes [1, 2, 3]
+stk.push(4);                          // stack still [1, 2, 3], Do not add another elements as size is 4
+stk.increment(5, 100);                // stack becomes [101, 102, 103]
+stk.increment(2, 100);                // stack becomes [201, 202, 103]
+stk.pop();                            // return 103 --> Return top of the stack 103, stack becomes [201, 202]
+stk.pop();                            // return 202 --> Return top of the stack 202, stack becomes [201]
+stk.pop();                            // return 201 --> Return top of the stack 201, stack becomes []
+stk.pop();                            // return -1 --> Stack is empty return -1.
