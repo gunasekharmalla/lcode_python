1531b15cc17132ecf7901eac7fee32c08258ec92
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-06-06 20:05:44 +0530

    Update 12.py

--- Arrays/medium/12.py
+++ Arrays/medium/12.py
@@ -1,1 +1,57 @@
+def intToRoman(num: int) -> str:
+    val = [
+        1000, 900, 500, 400, 
+        100, 90, 50, 40, 
+        10, 9, 5, 4, 1
+    ]
+    
+    syms = [
+        "M", "CM", "D", "CD", 
+        "C", "XC", "L", "XL", 
+        "X", "IX", "V", "IV", "I"
+    ]
+    
+    result = ""
+    
+    for i in range(len(val)):
+        while num >= val[i]:
+            num -= val[i]
+            result += syms[i]
+    
+    return result
 
+Example 1:
+
+Input: num = 3749
+
+Output: "MMMDCCXLIX"
+
+Explanation:
+
+3000 = MMM as 1000 (M) + 1000 (M) + 1000 (M)
+ 700 = DCC as 500 (D) + 100 (C) + 100 (C)
+  40 = XL as 10 (X) less of 50 (L)
+   9 = IX as 1 (I) less of 10 (X)
+Note: 49 is not 1 (I) less of 50 (L) because the conversion is based on decimal places
+Example 2:
+
+Input: num = 58
+
+Output: "LVIII"
+
+Explanation:
+
+50 = L
+ 8 = VIII
+Example 3:
+
+Input: num = 1994
+
+Output: "MCMXCIV"
+
+Explanation:
+
+1000 = M
+ 900 = CM
+  90 = XC
+   4 = IV
