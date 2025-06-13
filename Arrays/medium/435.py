d0a344d7867f13e8a99b20805843851fb6473a44
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-06-13 22:13:07 +0530

    Update 435.py

--- Arrays/medium/435.py
+++ Arrays/medium/435.py
@@ -1,1 +1,19 @@
+class Solution(object):
+    def eraseOverlapIntervals(self, intervals):
+        if not intervals:
+            return 0
+        intervals.sort(key=lambda x: x[1])
+        res = []
+        count = 0
+        last_end = intervals[0][1]
+        for start, end in intervals[1:]:
+            if last_end <= start:
+                last_end = end
+                continue
+            else:
+                count += 1
+                res.append([start,end])
+        return count
 
+
+        
